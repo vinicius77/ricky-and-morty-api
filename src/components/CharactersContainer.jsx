80063c02506b3fa1dfaef9c01d02f3841d9d2c9c
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2020-12-04 16:05:11 +0200

    First commit and cleaning up of the unnecessary files

---
+++ src/components/CharactersContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharactersContainer = () => {
+  return (
+    <section className="characters">
+      <h1>Hello</h1>
+    </section>
+  );
+};
+
+export default CharactersContainer;
