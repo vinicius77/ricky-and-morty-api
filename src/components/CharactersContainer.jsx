d95d9326d06f66e2e6cb13de42c1f4215c1ccab0
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2020-12-04 17:56:06 +0200

    Renders the state from Redux on the component maping both state and dispatch to props

--- src/components/CharactersContainer.jsx
+++ src/components/CharactersContainer.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchingData } from '../redux/actions/charactersActionTypes';
 
-const CharactersContainer = () => {
+const CharactersContainer = ({ characters, fetchData }) => {
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <section className="characters">
-      <h1>Hello</h1>
+      <h1>Ricky and Morty Characters</h1>
+      {characters.loading && <div>Loading ...</div>}
     </section>
   );
 };
 
-export default CharactersContainer;
+/** The mapStateToProps function in simple words, transforms the state coming from Redux
+ * in props, allowing to use the state in your application */
+const mapStateToProps = (state) => {
+  return { characters: state.characters };
+};
+
+/** The mapDispatchToProps function allows to dispatch actions which change
+ * the state of the application in the Redux store */
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: () => {
+      dispatch(fetchingData());
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CharactersContainer);
